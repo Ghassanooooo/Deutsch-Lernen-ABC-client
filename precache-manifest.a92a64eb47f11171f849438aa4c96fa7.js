be5ad10c9b32a84379dab12e4a39d153d1191b91
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-11-18 11:13:46 +0100

    Updates

---
+++ precache-manifest.a92a64eb47f11171f849438aa4c96fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ded6217afbe713c2f4ff",
+    "url": "/Deutsch-Lernen-ABC-client/static/js/runtime~main.ded6217a.js"
+  },
+  {
+    "revision": "7b3ccc1524e078700687",
+    "url": "/Deutsch-Lernen-ABC-client/static/js/main.7b3ccc15.chunk.js"
+  },
+  {
+    "revision": "2e0f0b83919644566799",
+    "url": "/Deutsch-Lernen-ABC-client/static/js/1.2e0f0b83.chunk.js"
+  },
+  {
+    "revision": "7b3ccc1524e078700687",
+    "url": "/Deutsch-Lernen-ABC-client/static/css/main.4b78d2c7.chunk.css"
+  },
+  {
+    "revision": "6beb51f94d605680a1cddc2ae6daacfb",
+    "url": "/Deutsch-Lernen-ABC-client/index.html"
+  }
+];
